c36066088140ac85b372ceda18993fd394b3fdf2
raquel <raquelpermarcat@outlook.com>
2019-10-28 13:32:48 -0300

    exercicios resolvidos

--- 00a_revisao/index.js
+++ 00a_revisao/index.js
@@ -1,7 +1,48 @@
 // 1.
-// [] adicionar a tag <script> no html
-// [] verificar se está lendo index.js
-// [] alterar cor de <body>
-// [] escutar o clique do botão
-// [] criar função de alterar a cor usando rgb()
-// [] usar Math.random() e Math.floor() ou parseInt()
+// [x] adicionar a tag <script> no html
+// [x] verificar se está lendo index.js
+// [x] alterar cor de <body>
+// [x] escutar o clique do botão
+// [x] criar função de alterar a cor usando rgb()
+// [x] usar Math.random() e Math.floor() ou parseInt()
+
+
+// console.log('ola');
+
+
+// const body = document.querySelector('body');
+// const body = document.getElementsByTagName('body');
+const body = document.body;
+console.log(body);
+
+
+ body.style.backgroundColor = 'purple';
+
+
+ //  console.log(intRandom);
+ 
+ const btn= document.getElementById('trocaCor');
+ console.log(btn);
+ btn.addEventListener('click' , mudaCor);
+
+ function geraInteiro() {
+     const n= parseInt(Math.random() * 255);
+     console.log(n);
+     return n;
+     
+ }
+ 
+ function mudaCor(){
+   
+    const r =geraInteiro(); 
+    const g = geraInteiro(); 
+    const b =geraInteiro(); 
+
+    body.style.backgroundColor = 'rgb('+r+',' +g+ ',' +b+')'
+    // console.log('entrei na function eeee');
+    // console.log(r,g,b);
+    // console.log('rgb(' +r+ ',' +g+ ',' +b+ ')');
+    
+    
+}
+ 
--- 00b_revisao/index.js
+++ 00b_revisao/index.js
@@ -8,7 +8,43 @@
 
 // ------------------------------------------------------------------------
 // 2. melhorando o código
-// [] remover repetição de código
+// [x] remover repetição de código
 // [] anonymous function
 // [] arrow function
 // [] template string
+
+
+
+
+
+
+const body = document.body;
+console.log(body);
+
+
+ body.style.backgroundColor = 'purple';
+ 
+
+
+ //  console.log(intRandom);
+ 
+ const btn= document.getElementById('trocaCor');
+ console.log(btn);
+ btn.addEventListener('click' ,()=>{
+   
+    const r =geraInteiro(255); 
+    const g = geraInteiro(255); 
+    const b =geraInteiro(255); 
+
+    body.style.backgroundColor = 'rgb('+r+',' +g+ ',' +b+')'
+    
+    
+    
+});
+
+  geraInteiro = num =>  parseInt(Math.random() * num);
+     
+ 
+ 
+ 
+ 
