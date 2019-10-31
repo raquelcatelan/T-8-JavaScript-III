1622a6add283633f0892528c302a28e22618eb3a
raquel <raquelpermarcat@outlook.com>
2019-10-31 16:48:12 -0300

    terminado exercicio de input e desenvolvimento do search

--- 04b_fetch_giphy_search/index.js
+++ 04b_fetch_giphy_search/index.js
@@ -5,3 +5,51 @@
 // [] usar o método fetch() para a url da API Giphy
 // [] transformar o dado recebido
 // [] apresentar <img>
+
+
+const input = document.querySelector('#gifInput');
+// console.log(input);
+const botao = document.querySelector('#gifButton');
+// console.log(botao);
+const div = document.querySelector('#root')
+
+botao.addEventListener('click' , (e) => fazerFetc(e));
+
+function fazerFetc (event) {
+    event.preventDefault();
+    const valorInput = input.value;
+    div.innerHTML = "";
+    input.value= "";
+    // console.log(valorInput);
+    const url = `http://api.giphy.com/v1/gifs/search?q=${valorInput}&api_key=dc6zaTOxFJmzC&limit=4&offset=0`
+
+
+    fetch(url)
+    .then(resposta => {
+        if (!resposta.ok) {
+            console.error(resposta.statusText);
+            mostrarErro(resposta.statusText);
+        }
+    })
+    .then(json => mostarResultado(json))
+    .catch(erro => mostrarErro(erro))
+
+
+}
+
+mostrarErro = (erro) => {
+    div.innerHTML= `<h1>${erro}</h1>`
+}
+
+function mostarResultado (json) {
+
+    const arr  = json.data
+    console.log(json.data);
+    arr.forEach(element => {
+        const srcImg =  element.images.original.url
+        div.innerHTML += `
+        <img src=${srcImg}>
+        `
+    });
+    
+}
